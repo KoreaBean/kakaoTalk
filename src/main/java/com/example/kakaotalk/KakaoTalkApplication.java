b23855f8d22eaac311d2e3f7b406a92a9ede264d
koreaBean <ksg04001@daum.net>
2024-05-09 10:52:51 +0900

    kakaoTalk - 졸업작품 세팅

---
+++ src/main/java/com/example/kakaotalk/KakaoTalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.kakaotalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakaoTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kakaotalk/KakaoTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kakaotalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
